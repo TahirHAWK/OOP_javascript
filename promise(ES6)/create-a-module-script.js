06da7893d589c7d941f8c3e0cee9c4927a0d4812
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-05-25 13:14:57 +0600

    practice in session

---
+++ promise(ES6)/create-a-module-script.js
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <!-- Only change code below this line -->
+
+    <!-- Only change code above this line -->
+    <script type="module" src="index.js"></script>
+  </body>
+</html>
